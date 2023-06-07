8de5c207f47e942d898b9772a6d3529a14ee1eb4
Filipe Ferreira <filipeferreira94@gmail.com>
2023-06-07 15:33:54 +0100

    Add login and session permanence functionality

--- routes/auth.routes.js
+++ routes/auth.routes.js
@@ -24,4 +24,45 @@ router.post('/signup', async (req, res, next) => {
 	}
 });
 
+//GET /login
+router.get('/login', (req, res, next) => {
+	res.render('auth/login');
+});
+
+//GET user-profile
+router.get('/user-profile', (req, res) => {
+	res.render('auth/user-profile.hbs', req.session.currentUser);
+});
+
+router.post('/login', async (req, res, next) => {
+	try {
+		const { email, password } = req.body;
+		if (email === '' || password === '') {
+			res.status(400).render('auth/login', {
+				errorMessage: 'Please enter both, email and password to login.',
+			});
+			return;
+		}
+
+		const user = await User.findOne({ email: email });
+
+		if (!user) {
+			//if we find no email like the one we get from the form, that promise returns null
+			res.status(400).render('auth/login', {
+				errorMessage: 'Email is not registered. Try again.',
+			});
+			return;
+		} else if (bcrypt.compareSync(password, user.passwordHash)) {
+			req.session.currentUser = user;
+			console.log(req.session.currentUser);
+			res.render('auth/user-profile', { user: req.session.currentUser });
+		} else {
+			res.send('Unexpected error.');
+		}
+	} catch (error) {
+		console.log(error);
+		next(error);
+	}
+});
+
 module.exports = router;
