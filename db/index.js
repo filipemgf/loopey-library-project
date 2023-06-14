ed5b7fc48f6f49f2154ae755be572ec5af2ce938
Filipe Ferreira <filipeferreira94@gmail.com>
2023-06-14 10:24:34 +0100

    Updated connection based on Atlas connection.

--- db/index.js
+++ db/index.js
@@ -1,11 +1,11 @@
 // ℹ️ package responsible to make the connection with mongodb
 // https://www.npmjs.com/package/mongoose
-const mongoose = require("mongoose");
+const mongoose = require('mongoose');
 
 // ℹ️ Sets the MongoDB URI for our app to have access to it.
 // If no env has been set, we dynamically set it to whatever the folder name was upon the creation of the app
 
-const MONGO_URI = process.env.MONGODB_URI || "mongodb://127.0.0.1:27017/library-project";
+/* const MONGO_URI = process.env.MONGODB_URI || "mongodb://127.0.0.1:27017/library-project";
 
 mongoose
   .connect(MONGO_URI)
@@ -16,3 +16,35 @@ mongoose
   .catch((err) => {
     console.error("Error connecting to mongo: ", err);
   });
+ */
+
+// Full code sample from MongoAtlas driver connection method
+
+const { MongoClient, ServerApiVersion } = require('mongodb');
+const uri =
+	'mongodb+srv://filipe:filipe123@cluster0.6l30n6f.mongodb.net/library-loopey-project?retryWrites=true&w=majority';
+
+// Create a MongoClient with a MongoClientOptions object to set the Stable API version
+const client = new MongoClient(uri, {
+	serverApi: {
+		version: ServerApiVersion.v1,
+		strict: true,
+		deprecationErrors: true,
+	},
+});
+
+async function run() {
+	try {
+		// Connect the client to the server	(optional starting in v4.7)
+		await client.connect();
+		// Send a ping to confirm a successful connection
+		await client.db('admin').command({ ping: 1 });
+		console.log(
+			'Pinged your deployment. You successfully connected to MongoDB!'
+		);
+	} finally {
+		// Ensures that the client will close when you finish/error
+		await client.close();
+	}
+}
+run().catch(console.dir);
