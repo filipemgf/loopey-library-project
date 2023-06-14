8a40ac0d702692d1cfa232e2ec4e290f9aecfc76
Filipe Ferreira <filipeferreira94@gmail.com>
2023-06-14 11:23:42 +0100

    Removed URI exposure

--- db/index.js
+++ db/index.js
@@ -17,34 +17,3 @@ mongoose
 	.catch((err) => {
 		console.error('Error connecting to mongo: ', err);
 	});
-
-// Full code sample from MongoAtlas driver connection method
-
-/* const { MongoClient, ServerApiVersion } = require('mongodb');
-const uri =
-	'mongodb+srv://filipe:filipe123@cluster0.6l30n6f.mongodb.net/library-loopey-project?retryWrites=true&w=majority';
-
-// Create a MongoClient with a MongoClientOptions object to set the Stable API version
-const client = new MongoClient(uri, {
-	serverApi: {
-		version: ServerApiVersion.v1,
-		strict: true,
-		deprecationErrors: true,
-	},
-});
-
-async function run() {
-	try {
-		// Connect the client to the server	(optional starting in v4.7)
-		await client.connect();
-		// Send a ping to confirm a successful connection
-		await client.db('admin').command({ ping: 1 });
-		console.log(
-			'Pinged your deployment. You successfully connected to MongoDB!'
-		);
-	} finally {
-		// Ensures that the client will close when you finish/error
-		await client.close();
-	}
-}
-run().catch(console.dir); */
