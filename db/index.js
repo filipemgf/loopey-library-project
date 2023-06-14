5da24fa3603100ed17d97db95f0c49aa8d27ca34
Filipe Ferreira <filipeferreira94@gmail.com>
2023-06-14 11:14:23 +0100

    Configured db connection using .env

--- db/index.js
+++ db/index.js
@@ -5,22 +5,22 @@ const mongoose = require('mongoose');
 // ℹ️ Sets the MongoDB URI for our app to have access to it.
 // If no env has been set, we dynamically set it to whatever the folder name was upon the creation of the app
 
-/* const MONGO_URI = process.env.MONGODB_URI || "mongodb://127.0.0.1:27017/library-project";
+const MONGO_URI =
+	process.env.MONGODB_URI || 'mongodb://127.0.0.1:27017/library-project';
 
 mongoose
-  .connect(MONGO_URI)
-  .then((x) => {
-    const databaseName = x.connections[0].name;
-    console.log(`Connected to Mongo! Database name: "${databaseName}"`);
-  })
-  .catch((err) => {
-    console.error("Error connecting to mongo: ", err);
-  });
- */
+	.connect(MONGO_URI)
+	.then((x) => {
+		const databaseName = x.connections[0].name;
+		console.log(`Connected to Mongo! Database name: "${databaseName}"`);
+	})
+	.catch((err) => {
+		console.error('Error connecting to mongo: ', err);
+	});
 
 // Full code sample from MongoAtlas driver connection method
 
-const { MongoClient, ServerApiVersion } = require('mongodb');
+/* const { MongoClient, ServerApiVersion } = require('mongodb');
 const uri =
 	'mongodb+srv://filipe:filipe123@cluster0.6l30n6f.mongodb.net/library-loopey-project?retryWrites=true&w=majority';
 
@@ -47,4 +47,4 @@ async function run() {
 		await client.close();
 	}
 }
-run().catch(console.dir);
+run().catch(console.dir); */
